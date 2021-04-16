848078fa2a424130a888e5477f2006d58d435ca8
Carlos Sibalatani <carlos.sibalatani@mhss.gov.na>
2021-04-16 15:05:14 +0200

    initial commit

---
+++ src/app.js
@@ -0,0 +1,90 @@
+const express = require('express')
+const path = require('path')
+const hbs = require('hbs')
+const geocode = require('../utils/geocode')
+const forecast = require('../utils/forecast')
+
+const app = express()
+const publicDirectory = path.join(__dirname, '../public')
+const viewPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+app.set('view engine','hbs')
+hbs.registerPartials(partialsPath)
+app.set('views', viewPath)
+
+
+app.use(express.static(publicDirectory))
+
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
+
+//render homepage
+app.get('',(req, res) =>{
+    res.render('index',{
+        title: 'Weather App',
+        name: 'Carlos Sibalatani'
+    })
+})
+app.get('/weather', (req,res) => {
+    
+    if (!req.query.address) {
+        return res.send({
+            error: 'You must provide an address'
+        })
+    }
+    geocode.geocode(req.query.address, (error, {latitude, longitude, location} = {}) => {
+        if (error) {
+            return res.send({
+                error
+            })
+        }
+        forecast.forecast(latitude,longitude, (error, {temperature, feelslike, weatherDescription}) => {
+            if (error) {
+                return res.send({
+                })
+            }
+            res.send({
+                location,
+                temperature,
+                feelslike,
+                weatherDescription
+            })
+        })
+    })
+})
+
+app.get('/about',(req,res) => {
+    res.render('about',{
+        title: 'About me',
+        name: 'Carlos Sibalatani'
+    })
+})
+
+app.get('/help', (req,res) => {
+    res.render('help',{
+        title: 'Help',
+        name: 'Carlos Sibalatani',
+        message: 'Contact sibalatanics@outlook.com'
+    })
+})
+
+app.get('/help/*',(req, res) => {
+    res.render('404',{
+        title: "404 - Page not found",
+        message: "Sorry, an error has occured, Requested Help page not found!",
+        name: 'Carlos Sibalatani'
+    })
+})
+
+app.get('*',(req,res) => {
+    res.render('404',{
+        title: '404 - Page not found',
+        message: 'Sorry, an error has occured, Requested page not found!',
+        name: 'Carlos Sibalatani'
+    })
+})
+app.listen(3000, () =>{
+    console.log('Server is up on port 3000.')
+})
